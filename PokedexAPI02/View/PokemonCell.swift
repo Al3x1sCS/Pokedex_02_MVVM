b197aacb9a0e1908cf8b6d53d4fd6198af21a3a4
Al3x1s <prog.alexis.cs@protonmail.com>
2022-04-14 14:13:01 -0500

    APP Face inicial

---
+++ PokedexAPI02/View/PokemonCell.swift
@@ -0,0 +1,22 @@
+//
+//  PokemonCell.swift
+//  PokedexAPI02
+//
+//  Created by user216592 on 4/14/22.
+//
+
+import SwiftUI
+
+struct PokemonCell: View {
+    var body: some View {
+        ZStack {
+            
+        }
+    }
+}
+
+struct PokemonCell_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
