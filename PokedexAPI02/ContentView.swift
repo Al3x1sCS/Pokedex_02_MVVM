328e07511e1384b40d0a55c4733c1092f91b5451
Al3x1s <prog.alexis.cs@protonmail.com>
2022-04-14 13:58:15 -0500

    Initial Commit

---
+++ PokedexAPI02/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PokedexAPI02
+//
+//  Created by user216592 on 4/14/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PokedexAPI02/PokedexAPI02App.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexAPI02App.swift
+//  PokedexAPI02
+//
+//  Created by user216592 on 4/14/22.
+//
+
+import SwiftUI
+
+@main
+struct PokedexAPI02App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
